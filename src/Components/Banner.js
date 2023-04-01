8c402542d72becda06a2443ba36306205f18c8aa
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-04-01 05:47:04 +0300

    Add hero section

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,9 +1,14 @@
 import './css/Banner.css';
+import bg from '../img/bg.png';
 
 function Banner() {
   return (
     <div className='wrapper'>
-      <div className="banner"></div>
+      <div className="banner">
+      <img src={bg} alt="background" className='banner__bg'/>
+        <h1 className='banner__title'>Твоя первая <span>гитара</span> ждет <span>тебя</span></h1>
+        <p className='banner__subtitle'>Мы сделаем из вас музыкантов</p>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './Components/Header';
+import Banner from './Components/Banner';
 import './Components/css/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <Banner />
   </React.StrictMode>
 );
